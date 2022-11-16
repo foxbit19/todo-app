3edc2ff76a975bd9864c1655bc5150b28dc13564
foxbit19 <foxbit19@users.noreply.github.com>
2022-11-16 14:12:21 +0100

    Improve server and server_test

--- server/src/server.go
+++ server/src/server.go
@@ -3,8 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
-func TodoServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "first todo")
+type ItemStore interface {
+	GetTodoDescription(id int) string
+}
+
+type TodoServer struct {
+	store ItemStore
+}
+
+func (s *TodoServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(strings.TrimPrefix(r.URL.Path, "/items/"))
+
+	fmt.Fprint(w, s.store.GetTodoDescription(id))
 }
--- server/src/server_test.go
+++ server/src/server_test.go
@@ -1,25 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func TestGETItem(t *testing.T) {
+type StubItemStore struct {
+	todo map[int] string
+}
+
+func (s *StubItemStore) GetTodoDescription(id int) string {
+	return s.todo[id]
+}
+
+func TestGETTodoItem(t *testing.T) {
+	store := StubItemStore{
+		map[int]string{
+			1: "this is my first todo",
+			2: "this is my second todo",
+		},
+	}
+	server := &TodoServer{&store}
+
 	t.Run("returns the first todo item", func (t *testing.T) {
-		// Creates a new GET request on "items" without a body (nil)
-		request, _ := http.NewRequest(http.MethodGet, "items", nil)
+		request := newGetTodoRequest(1)
 		// this is the spy
 		response := httptest.NewRecorder()
 
-		TodoServer(response, request)
+		server.ServeHTTP(response, request)
 
-		got := response.Body.String()
-		want := "first todo"
+		assertResponseBody(t, response.Body.String(), "this is my first todo")
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
 	})
+	t.Run("returns the second todo item", func (t *testing.T) {
+		request := newGetTodoRequest(2)
+		// this is the spy
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertResponseBody(t, response.Body.String(), "this is my second todo")
+	})
+}
+
+func newGetTodoRequest(id int) *http.Request {
+	// Creates a new GET request on "items" without a body (nil)
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/items/%d", id), nil)
+	return request;
+}
+
+func assertResponseBody(t *testing.T, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
