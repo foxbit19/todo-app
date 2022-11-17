674e19e46e6c050413956e3bf3878d990849b4f6
foxbit19 <foxbit19@users.noreply.github.com>
2022-11-17 15:52:23 +0100

    Replace old server init function

--- server/src/main.go
+++ server/src/main.go
@@ -5,11 +5,12 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/foxbit19/todo-app/server/src/api"
 	"github.com/foxbit19/todo-app/server/src/store"
 )
 
 func main()  {
-	server := &TodoServer{&store.InMemoryItemStore{}}
+	server := api.NewTodoServer(&store.InMemoryItemStore{})
 	const port int = 8000
 	fmt.Printf("Starting server at port %d\n",port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server))
