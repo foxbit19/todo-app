5650f255d704e891ef612bcc3ccd47a6e14f9159
foxbit19 <foxbit19@users.noreply.github.com>
2022-11-18 17:09:39 +0100

    Improve main with database load

--- server/src/main.go
+++ server/src/main.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/foxbit19/todo-app/server/src/api"
 	"github.com/foxbit19/todo-app/server/src/store"
 )
 
+const dbFileName = "todo.db.json"
+const port = 8000
+
 func main()  {
-	server := api.NewTodoServer(&store.InMemoryItemStore{})
-	const port int = 8000
-	fmt.Printf("Starting server at port %d\n",port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), server))
+	file, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("Error on create or open database %s: %v", dbFileName, err)
+	}
+
+	server := api.NewTodoServer(&store.FileSystemStore{file})
+
+	log.Printf("Starting ToDo server on port %d",port)
+
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), server); err != nil {
+		log.Fatalf("Could not listen on port %d %v", port, err)
+	}
+
 }
