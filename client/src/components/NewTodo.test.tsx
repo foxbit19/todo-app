1768f5faeea40cefb8b9b0f95fd0c84afc0cfde2
foxbit19 <foxbit19@users.noreply.github.com>
2022-11-19 16:17:45 +0100

    Remove cypress. Add component test

--- client/src/components/NewTodo.test.tsx
+++ client/src/components/NewTodo.test.tsx
@@ -3,12 +3,34 @@ import { render, screen } from '@testing-library/react';
 import NewTodo from './NewTodo';
 import userEvent from '@testing-library/user-event';
 
-test('it clears the input field once save button is clicked', () => {
-    render(<NewTodo />);
+describe('new todo component', () => {
+    const getInputElement = (): HTMLInputElement => {
+        return screen.getByTestId('todo_description')
+    }
 
-    const input: HTMLInputElement = screen.getByTestId('todo_description')
-    userEvent.type(input, 'wonderful!')
-    const button: HTMLInputElement = screen.getByTestId('todo_save')
-    userEvent.click(button)
-    expect(input.value).toEqual('');
+    const typeDescription = (description: string) => {
+        userEvent.type(getInputElement(), description)
+    }
+
+    const clickSaveButton = () => {
+        const button: HTMLInputElement = screen.getByTestId('todo_save')
+        userEvent.click(button)
+    }
+
+    test('it clears the input field once save button is clicked', () => {
+        render(<NewTodo />);
+        typeDescription('wonderful!')
+        clickSaveButton()
+
+        expect(getInputElement().value).toEqual('');
+    })
+
+    test('it shows the items into page after save', () => {
+        render(<NewTodo />);
+        typeDescription('my todo item')
+        clickSaveButton()
+
+        const item = screen.getByText(/my todo item/i);
+        expect(item).toBeInTheDocument();
+    })
 })
--- client/src/components/NewTodo.tsx
+++ client/src/components/NewTodo.tsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react'
+import React, { DetailedHTMLProps, InputHTMLAttributes, useState } from 'react'
 
 type Props = {}
 
 const NewTodo = (props: Props) => {
-    const [value, setValue] = useState<string>('')
+    const [inputValue, setInputValue] = useState<string | undefined>('')
+    const [item, setItem] = useState<string | undefined>('')
+
+    const handleChange = (event: any) => {
+        setInputValue(event.target.value)
+    }
 
     const handleSave = () => {
-        setValue('')
+        setItem(inputValue)
+        setInputValue('')
     }
 
     return <div>
-        <input data-testid='todo_description' value={value} />
+        <input data-testid='todo_description' value={inputValue} onChange={handleChange} />
         <button data-testid='todo_save' onClick={handleSave}>Save</button>
+        <div>{item}</div>
     </div>
 }
 
