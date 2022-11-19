58fb5b1281b0ece5f8dd3667d36dde95de19a409
foxbit19 <foxbit19@users.noreply.github.com>
2022-11-19 16:28:32 +0100

    Add title component

---
+++ client/src/components/title/Title.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+const Title = (props: Props) => {
+    return <div>
+        <div>ToDo App</div>
+        <div>The perfect solution for yours day to day ToDos</div>
+    </div>
+}
+
+export default Title
