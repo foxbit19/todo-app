967c0b44f59e60779f97794fd23ccbca552660b8
foxbit19 <foxbit19@users.noreply.github.com>
2022-11-20 16:30:21 +0100

    Improve todo list and update snapshot

--- client/src/components/list/TodoList.tsx
+++ client/src/components/list/TodoList.tsx
@@ -1,38 +1,29 @@
 import React from 'react'
-import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import { List } from '@mui/material'
 import Item from '../../models/item'
-import DoneIcon from '@mui/icons-material/Done'
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
+import TodoItem from '../item/TodoItem';
+
 interface Props {
     items: Item[]
     onItemClick?: (item: Item) => void
+    onComplete?: (item: Item) => void
 }
 
 const TodoList = (props: Props) => {
-    const normalizeDescription = (description: string) => {
-        return description.length > 50 ? `${description.substring(0, 50)}...` : description
-    }
-
     return (
         <DragDropContext onDragEnd={() => { }}>
             <Droppable droppableId='1'>
                 {(provided) => <List data-testid="list" ref={provided.innerRef} {...provided.droppableProps}>
                     {props.items.map((item: Item, index: number) => (
-                        <Draggable draggableId={item.id.toString()} index={index}>
-                            {(provided) => (
-                                <ListItemButton key={index} data-testid={`todo_${item.id}`} onClick={() => props.onItemClick && props.onItemClick(item)} ref={provided.innerRef}
-                                    {...provided.draggableProps}
-                                    {...provided.dragHandleProps}>
-                                    <ListItemIcon><DoneIcon /></ListItemIcon>
-                                    <ListItemText>{normalizeDescription(item.description)}</ListItemText>
-                                </ListItemButton>
-                            )}
-                        </Draggable>
+                        <TodoItem index={index} item={item}
+                            onClick={() => props.onItemClick && props.onItemClick(item)}
+                            onComplete={() => props.onComplete && props.onComplete(item)} />
                     ))}
-                    {props.items.length === 0 ? <div>There are no todos to show</div> : ''}
                     {provided.placeholder}
                 </List >}
             </Droppable>
+            {props.items.length === 0 ? <div>There are no todos to show</div> : ''}
         </DragDropContext>
     )
 }
